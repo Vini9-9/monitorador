89257e29e84a04994ffebe3515a7503117579df8
Vinicius Pessoa <vipdsilva@ibm.com>
2021-11-02 22:27:01 -0300

    Monitoramento a cada segundo

--- stats.js
+++ stats.js
@@ -1,23 +1,27 @@
 // importando um módulo dentro do Node
 const os = require('os');
 
-console.log(os.platform());
+function getDadosOS (){
 
-const { freemem , totalmem } = os;
+    const { freemem , totalmem } = os;
 
-// cálculo para MB
-function toMB(number) {
-    return (number / 1024 / 1024);
+    // cálculo para MB
+    function toMB(number) {
+        return (number / 1024 / 1024);
+    }
+
+    const total = parseInt(toMB(totalmem()));
+    const mem = parseInt(toMB(freemem()));
+    const percents = parseInt((mem / total) * 100);
+
+    const stats = {
+        livre: `${mem} MB`,
+        total: `${total} MB`,
+        usado: `${percents} %`
+    }
+    console.clear();
+    console.table(stats);
 }
 
-const total = parseInt(toMB(totalmem()));
-const mem = parseInt(toMB(freemem()));
-const percents = parseInt((mem / total) * 100);
-
-const stats = {
-    livre: `${mem} MB`,
-    total: `${total} MB`,
-    usado: `${percents} %`
-}
-
-console.table(stats);
+// Utilizando o EventLoop
+setInterval(getDadosOS, 1000);
