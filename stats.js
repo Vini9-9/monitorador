39dc15b52c59f5f71efc374a5d475996411f16b0
Vinicius Pessoa <vipdsilva@ibm.com>
2021-11-03 00:15:55 -0300

    Log para o input do usuário

--- stats.js
+++ stats.js
@@ -43,13 +43,16 @@ console.log("==================================================");
 
 leitor.question("Qual a frequência desejada do monitoramento em segundos?\n", function(answer) {
     var resp = answer;
+
+    log(`Input do usuário :  ${resp}\n`);
+
     resp = (resp <= 0 | isNaN(resp) ? 1 : resp);
     segundos = parseInt(resp)* 1000;
 
-    console.log("\Os dados serão processados daqui '" + segundos/1000 + "' segundos");
-    leitor.close();
-
     log("frequência : " + segundos/1000 + " segundos\n");
 
+    console.log("\Os dados serão processados daqui '" + segundos/1000 + "' segundos");
+    leitor.close();
+    
     setInterval(getDadosOS, segundos);
 });
